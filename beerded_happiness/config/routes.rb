cdcedc445d6b8ac34c3a8a17446591e096f6e7f0
steph t. zhang <stephtzhang@gmail.com>
2014-10-10 17:04:23 -0400

    add login session

--- beerded_happiness/config/routes.rb
+++ beerded_happiness/config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   resources :users
 
   # get '/login' => 'rasession#index'
-  root 'session#index'
+  root 'sessions#index'
+
+  get '/login' => 'sessions#login'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
