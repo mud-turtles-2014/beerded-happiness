0953f4547b7d54d7a00e9ea150c0f92ce0dad434
Michelle Chu <michu@Michelles-MacBook-Pro-2.local>
2014-10-11 11:48:37 -0400

    Add show events page

--- beerded_happiness/app/controllers/events_controller.rb
+++ beerded_happiness/app/controllers/events_controller.rb
@@ -1,1 +1,13 @@
-events_controller.rb
+class EventsController < ApplicationController
+
+  def index
+    @active_events = Event.where(status: "active")
+    @inactive_events = Event.where(status: "inactive")
+    @past_events = Event.where(status: "past")
+  end
+
+  def new
+    @event = Event.new
+  end
+
+end
