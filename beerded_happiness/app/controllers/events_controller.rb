9c42e4de50b32724cf7e61003c82216698985f6d
Michelle Chu <michu@Michelles-MacBook-Pro-2.local>
2014-10-11 12:54:26 -0400

    Add create event functionality with tests

--- beerded_happiness/app/controllers/events_controller.rb
+++ beerded_happiness/app/controllers/events_controller.rb
@@ -1,13 +1,25 @@
 class EventsController < ApplicationController
 
   def index
-    @active_events = Event.where(status: "active")
-    @inactive_events = Event.where(status: "inactive")
-    @past_events = Event.where(status: "past")
+    @active_events = Event.where(status: "Active")
+    @inactive_events = Event.where(status: "Inactive")
+    @past_events = Event.where(status: "Past")
   end
 
   def new
     @event = Event.new
+  end
+
+  def create
+    Event.create(event_params)
+
+    redirect_to events_path
+  end
+
+  private
+
+  def event_params
+    params.require(:event).permit(:title, :status, :creator_id)
   end
 
 end
--- beerded_happiness/spec/controllers/event_spec.rb
+++ beerded_happiness/spec/controllers/event_spec.rb
@@ -20,4 +20,21 @@ describe EventsController do
     end
   end
 
+   describe "#create" do
+    it "adds a new event to the database" do
+      post :create, event: {title: "DBC BP", status: "Active", creator_id: user.id}
+      expect(Event.where(title: "DBC BP")).to exist
+    end
+
+    it "redirects events index page" do
+      post :create, event: {title: "BeerPong Fest", status: "Active", creator_id: user.id}
+      response.should redirect_to events_path
+    end
+
+    # it "renders form with new event" do
+    #   get :new
+    #   expect(assigns(:event)).to be_a_new Event
+    # end
+  end
+
 end
