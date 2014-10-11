0e9068b87c02d37227613095d7914fc500212516
Michelle Chu <michu@Michelles-MacBook-Pro-2.local>
2014-10-11 15:30:04 -0400

    Add refactoring before_load

--- beerded_happiness/app/controllers/events_controller.rb
+++ beerded_happiness/app/controllers/events_controller.rb
@@ -1,4 +1,5 @@
 class EventsController < ApplicationController
+  before_action :find_event, only: [:show, :edit, :update, :destroy]
 
   def index
     @active_events = Event.where(status: "Active")
