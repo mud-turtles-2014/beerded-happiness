2016cc849462a833286b2a091f01bf90ca9d91fa
Chris Wallwork <chris.wallwork@gmail.com>
2014-10-11 19:37:34 -0400

    recommiting files

--- beerded_happiness/app/controllers/games_controller.rb
+++ beerded_happiness/app/controllers/games_controller.rb
@@ -6,4 +6,12 @@ class GamesController < ActionController::Base
 	def show
 		@game = Game.find(params[:id])
 	end
+
+	def create
+		@event = Event.find(params[:event_id])
+		@game  = Game.create(status:"pending")
+		@game.users << User.find(session[:user_id])
+		@event.games << @game
+		redirect_to event_path(@event)
+	end
 end
